260cf86fba4ea69ec69a792b1e337225e2b4cae8
Denny Schaedig <dennys@wustl.edu>
2025-10-20 16:50:47 -0600

    Changing endpoint to api endpoint

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ def upload_json():
     # ---- Forward to API ----
     try:
         resp = requests.post(
-            "https://flask.jib-jab.org/upload_json",
+            "https://flask.jib-jab.org/api/upload",
             files={"jsonFile": (filename, augmented_bytes)},
             headers={"x-api-key": API_KEY},
             timeout=10,
